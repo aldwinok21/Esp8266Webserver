420f47b3656dd2223342c35c39482d6a7f9d6e68
Aldwin C. Tablante <aldwinotablante@gmail.com>
2021-07-16 18:32:35 +0800

    Access point server

--- sketch_jul02a.ino
+++ sketch_jul02a.ino
@@ -2,28 +2,51 @@
 #include <ESP8266WebServer.h>
 #include <WiFiClientSecure.h>
 
+
 #include "webpage.h";
 #include "httpmanager.h";
-// Wifi Name
-#define ssid "Aldwinok"
-// Wifi Password
-#define password "twoguysbuilders"
+
+#define server_ssid  "ESP8266"
+#define server_pass  ""
 
 ESP8266WebServer server(80);
 
 String deviceConnected = "";
 String tokenRecieved = "";
 String tokenRefresh = "";
-
-
+String ssid = "";
+String password = "";
+String statusconnect = "";
 String header;
 unsigned long currentTime = millis();
 
 //Return Webpage data
 void webpage(){
- 
-    Serial.println("A client has connected... \n");
-  server.send(200,"text/html",pageData(WiFi.localIP()));
+  if(WiFi.status() == WL_CONNECTED){
+       server.send(200,"text/html",pageData(WiFi.localIP()));
+    }
+    else{
+         server.send(200,"text/html",wifiPage(WiFi.localIP()));
+    }
+   delay(1000);
+}
+
+//Return Wifi page
+void wifipage(){
+  server.send(200,"text/html",wifiPage(WiFi.localIP()));
+  delay(1000);
+
+}
+
+//Return server info
+
+void wifiInfo(){
+  String statusw = "Not Connected";
+  if(WiFi.status() == WL_CONNECTED){
+    statusw = "Connected";
+    
+  }
+  server.send(200,"json","{\"token\":" +tokenRecieved+",\"ip_address\":"+WiFi.localIP().toString()+", \"status\":"+statusw+",\"w_ssid\":"+server_ssid+",\"apip\":"+WiFi.softAPIP().toString()+",\"c_ssid\":"+ssid+"}");
 }
 
 // Update Token for Withings...
@@ -31,50 +54,111 @@ void set_token(){
   Serial.println("Incoming token request..");
   if(server.hasArg("token") && server.arg("token") != NULL){
     tokenRecieved = server.arg("token");
-    server.send(200,"text/plain","{'success':1}");
-    Serial.println("Token Value :" + tokenRecieved);
+    server.send(200,"json","{\"success\":1}");
     }
     else{
       Serial.println("Client Invalid Request.");
-          server.send(400,"text/plain","400: Invalid Request");
+    server.send(200,"json","{\"success\":0}");
 
     }
+  delay(1000);
 }
 
-void setup() {
-  WiFi.mode(WIFI_STA);
+//Update Wifi Credentials
+void set_wifi_creds(){
+    Serial.println("Wifi credential request..");
+  if(server.hasArg("user") && server.arg("user") != NULL && server.hasArg("pass") && server.arg("pass") != NULL){
+    ssid = server.arg("user");
+    password = server.arg("pass");
+
+    // Check if Connected to wifi
+    if(wifi_connection()){
+          server.send(200,"text/plain","{\"success\":1,\"ip_address\":\""+WiFi.localIP().toString()+"\"}");
+          Serial.println("Wifi SSID :" + ssid );
+      }
+      else{
+      Serial.println("Client Invalid Request.");
+    server.send(200,"json","{\"success\":0}");
+      }
+    }
+    else{
+      Serial.println("Client Invalid Request.");
+    server.send(200,"json","{\"success\":0}");
+
+    }
+
+}
+
+// Set up Wifi Access point
+void wifi_access_point(){
+WiFi.softAP(server_ssid,server_pass);
+IPAddress IP = WiFi.softAPIP();
+
+
+}
+
+// Connect to wifi
+bool wifi_connection(){
+  //number of reconnecting tries count max 20
   WiFi.begin(ssid,password);
-
-  Serial.begin(115200);
-
-  while(WiFi.status() != WL_CONNECTED){
+  int tries = 0;
+  int max_count = 20;
+  
+  while(WiFi.status() != WL_CONNECTED and tries < max_count){
     delay(500);
     Serial.print(".");
+    tries++;
   }
-
+  
+  if(WiFi.status() == WL_CONNECTED){
   Serial.println("Connected to ");
   Serial.println(ssid);
   Serial.print("IP ADDRESS ");
   Serial.println(WiFi.localIP());
-  Serial.println("DateTime : " + currentTime);
+  statusconnect = "connected";
+  return true;
+  }
+  else{
+      Serial.println("Failed to connect.");
+      statusconnect = "disconnected";
+      return false;
+  }
+}
+
+void setup() {
+  WiFi.mode(WIFI_STA);
+  Serial.begin(115200);
+  wifi_access_point();
 
   server.on("/",webpage);
+  server.on("/settings",wifipage);
+
+  server.on("/serverInfo",HTTP_POST,wifiInfo);
+
   server.on("/setToken",HTTP_POST,set_token);
+  server.on("/setWifi",HTTP_POST,set_wifi_creds);
+
+
   server.begin();
+
+  Serial.println("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  if(WiFi.status() == WL_CONNECTED){
-    
-    
   server.handleClient();
+  if(WiFi.status() == WL_CONNECTED){
   if(tokenRecieved != ""){
+    // function found in httpmanager.h
     call_sleep_summary(tokenRecieved);
     delay(5000);
     }
   }
   else{
-    Serial.println("Failed connecting to WiFi");
+    //Serial.println("Failed connecting to WiFi. Reconnecting...");
+    //wifi_connection();
+          //wifi_connection();
+
   }
 }
--- webpage.h
+++ webpage.h
@@ -6,7 +6,7 @@ R"====(
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-  <!---------- CSS --------->
+  <!--- CSS --------->
   <style>
   .column {
     display:flex;
@@ -20,7 +20,7 @@ R"====(
     width:100%;
     height:100%;
   }
-  input[type="button"], input[type="submit"],input[type="text"]{
+  input[type="button"], input[type="submit"],input[type="text"],input[type="password"]{
     min-width:100px;
     padding:5px;
   }
@@ -40,13 +40,11 @@ R"====(
     min-width:200px;
     min-height:200px;
     padding:10px;
-    border: 1px solid seagreen;
-    border-radius: 10px;
     background-color:white;
   }
   body{
-    min-height:100vh;
-    background-color:lightblue;
+    min-height:90vh;
+    background:whitesmoke
   }
   .d-flex{
     display:flex;
@@ -57,80 +55,241 @@ R"====(
   label{
     font-size:12px;
   }
-  
+
   .mt-10{
     margin-top:10px;
   }
-  
+
   :root{
     font-family:sans-serif;
   }
-  bold{
+  .bold{
     font-weight:bold;
   }
-
+  .shadow{
+    box-shadow:0px 0px 1px #646262
+  }
   .custom-inp {
     outline:none;
     border:none;
     background:lightblue;
   }
-
  .custom-inp:focus{
   background:skyblue;
  }
-
+ a{
+   text-decoration: none;
+   font-size:12px;
+ }
+ a:active{
+   color:whitesmoke
+ }
   </style>
-  <!---------- CSS --------->
-
+  <!--- CSS --------->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <title>ESP8266</title>
   </head>
-  <body >
-  <div class="container d-flex justify-center">
-  <form type="post" id="tokenres" class="column justify-center input-holder "> 
-  <p class="bold">ESP8266 Settings</p>
-  <label for="but1">Input token text</label>
+  <body class="d-flex justify-center align-center" >
+    <div class="column">
+
+  <div class="container row d-flex justify-center ">
+  <form type="post" id="tokenres" class="column justify-center input-holder shadow">
+  <p class="bold">Server Settings</p>
+  <label for="tokentext" >Input token text</label>
   <input type="text" class="custom-inp" id="tokentext" value="" placeholder="Place token here" required>
+  <a href="https://customaticparts.com/servicePortal/app/api/" target="_blank" class="mt-10" style="font-size: ">Get Token Here</a>
   <input type="submit" id="but1" class="mt-10" value="Update Token" name="click" ></input>
   </form>
   </div>
-  
-  <!---------- JS --------->
+</div>
+
+
+  <!--- JS --------->
   <script>
   window.addEventListener('load',(event)=>{
-
     let but1 = document.getElementById("but1");
     let tokenres = document.getElementById("tokenres");
- 
+
+
       tokenres.addEventListener('submit',(e)=>{
         e.preventDefault();
         let input_data = document.getElementById("tokentext").value;
+
         setToken((dataResp)=>{
           console.log(dataResp.responseText);
           },input_data);
         })
 
-    
     })
-
     const setToken = function(callback,token){
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = () => {
         if(xhttp.readyState == 4 && xhttp.status == 200){
           callback(xhttp);
           }
-        } 
+        }
         xhttp.open("POST","./setToken",true);
         xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
         xhttp.send("token="+token);
     }
   </script>
-  <!---------- JS --------->
-
+  <!-- JS --------->
   </body>
-</html>)====";
+</html>
+
+)====";
+
+
+return webpageCode;
+}
+
+
+
+//Wifi Settings
+String wifiPage(IPAddress ip){
+const char webpageCode[] = 
+R"====(
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  <!--- CSS --------->
+  <style>
+  .column {
+    display:flex;
+    flex-direction:column;
+  }
+  .row {
+    display:flex;
+    flex-direction:row;
+  }
+  .container{
+    width:100%;
+    height:100%;
+  }
+  input[type="button"], input[type="submit"],input[type="text"],input[type="password"]{
+    min-width:100px;
+    padding:5px;
+  }
+  .all-center{
+    align-items:center;
+    align-content:center;
+    justify-content:center;
+  }
+  .align-center{
+    align-items:center;
+    align-content:center;
+  }
+  .justify-center{
+    justify-content:center;
+  }
+  .input-holder{
+    min-width:200px;
+    min-height:200px;
+    padding:10px;
+    background-color:white;
+  }
+  body{
+    min-height:90vh;
+    background:whitesmoke
+  }
+  .d-flex{
+    display:flex;
+  }
+  .inherit-h{
+    height:inherit !important;
+  }
+  label{
+    font-size:12px;
+  }
+
+  .mt-10{
+    margin-top:10px;
+  }
+
+  :root{
+    font-family:sans-serif;
+  }
+  .bold{
+    font-weight:bold;
+  }
+  .shadow{
+    box-shadow:0px 0px 1px #646262
+  }
+  .custom-inp {
+    outline:none;
+    border:none;
+    background:lightblue;
+  }
+ .custom-inp:focus{
+  background:skyblue;
+ }
+ a{
+   text-decoration: none;
+   font-size:12px;
+ }
+ a:active{
+   color:whitesmoke
+ }
+  </style>
+  <!--- CSS --------->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ESP8266</title>
+  </head>
+  <body class="d-flex justify-center align-center" >
+    <div class="column">
+  <div class="container row d-flex justify-center ">
+  <form type="post" id="wifires" class="column justify-center input-holder shadow" >
+  <p class="bold">Server Settings</p>
+  <label for="wiuser">Input Wifi SSID/Name</label>
+  <input type="text" class="custom-inp" id="wiuser" value="" placeholder="Wifi Name" required >
+  <label for="wipass" class="mt-10">Input Wifi Password</label>
+  <input type="password" class="custom-inp" id="wipass" value="" placeholder="Wifi password" required >
+  <input type="submit" id="but1" class="mt-10" value="Update Wifi" ></input>
+  </form>
+  </div>
+</div>
+  <!--- JS --------->
+  <script>
+  window.addEventListener('load',(event)=>{
+    let but1 = document.getElementById("but1");
+    let wifires = document.getElementById("wifires");
+      wifires.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        let input_data = document.getElementById("wiuser");
+        let input_pass = document.getElementById("wipass");
+        input_data.disabled = true;
+        input_pass.disabled = true;
+        setWifi((dataResp)=>{
+                 var data = JSON.parse(dataResp.responseText)
+                 if(data["success"]){
+                  alert("Server successfully connected to wifi --- Server New IP : " + data["ip_address"]);
+                 }
+                 else{
+                   alert("Server fail to Connect into wifi --- Please try again.");
+                 }
+                  window.location.reload();
+               },input_data.value,input_pass.value);
+
+    })
+  })
+    const setWifi = function(callback,user,pass){
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = () => {
+        if(xhttp.readyState == 4 && xhttp.status == 200){
+          callback(xhttp);
+          }  }
+        xhttp.open("POST","./setWifi",true);
+        xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        xhttp.send("user="+user+"&pass="+pass);
+    }
+  </script>
+  <!-- JS --------->
+  </body>
+</html>
+
+)====";
 
 
 return webpageCode;
